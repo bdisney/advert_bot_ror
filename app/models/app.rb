8538b87826c539e8aa49a7af9ffcf6f0cece87be
disne_y <bold.disney@gmail.com>
2017-07-11 17:37:24 +0300

    Add block_types and platrom_id to strong params. Validates it.

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,5 +1,7 @@
 class App < ApplicationRecord
   has_many :accounts_apps
   has_many :accounts, through: :accounts_apps
-  validates :name, :url, presence: true
+  validates :name, :url, :platform_id, :block_types, presence: true
+
+  AD_UNIT_TYPE = { standard: 'Standard', medium: 'Medium', leaderboard: 'Leaderboard' }.freeze
 end
