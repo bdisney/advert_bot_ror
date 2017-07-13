e2bfc992c1c1319d5e6b0368543c84fbc0b7b64a
disne_y <bold.disney@gmail.com>
2017-07-13 16:50:16 +0300

    Validate uniqueness of email

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe Account, type: :model do
   it { should have_many :apps }
 
   it { should validate_presence_of :email }
+  it { should validate_uniqueness_of :email }
   it { should validate_presence_of :password }
 end
